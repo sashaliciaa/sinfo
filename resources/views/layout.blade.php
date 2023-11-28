56ca50cff45c25af023fa5185cd69e17bd61842e
Zahrina Nurpuriani <zahrinanurpuriani25@gmail.com>
2023-11-28 08:54:31 +0700

    commit nav

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -79,7 +79,7 @@
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
                             data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                             <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                            Event Desa
+                            Agenda Kegiatan Desa
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
