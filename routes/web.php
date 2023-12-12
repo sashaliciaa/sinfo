b4e11f9070932b04d4aa34a3f8355d02a1bb51b4
Zahrina Nurpuriani <zahrinanurpuriani25@gmail.com>
2023-12-12 19:05:02 +0700

    landingpage controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
-use App\Http\Controllers\AgendaKegiatanController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GaleriController;
 use App\Http\Controllers\JabatanController;
-use App\Http\Controllers\PerangkatDesaController;
-use App\Http\Controllers\PerkebunanController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PerikananController;
 use App\Http\Controllers\PertanianController;
+use App\Http\Controllers\PerkebunanController;
 use App\Http\Controllers\PeternakanController;
+use App\Http\Controllers\PerangkatDesaController;
+use App\Http\Controllers\AgendaKegiatanController;
+use App\Http\Controllers\LandingpageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,20 +24,25 @@ use App\Http\Controllers\PeternakanController;
 |
 */
 
-Route::get('/', function () {
-    return view('landingpage.index');
-});
-
 Auth::routes();
 
+Route::resource('/', LandingpageController::class);
+Route::resource('/sindang-mekar', LandingpageController::class);
+Route::resource('/sindangmekar', LandingpageController::class);
+
 Route::resource('dashboard', DashboardController::class);
+
 Route::resource('jabatan', JabatanController::class);
+
 Route::resource('perangkatdesa', PerangkatDesaController::class);
+
 Route::resource('agendakegiatan', AgendaKegiatanController::class);
+
 Route::resource('/pekerjaan_desa/pertanian', PertanianController::class);
 Route::resource('/pekerjaan_desa/perkebunan', PerkebunanController::class);
 Route::resource('/pekerjaan_desa/peternakan', PeternakanController::class);
 Route::resource('/pekerjaan_desa/perikanan', PerikananController::class);
+
 Route::resource('/galeri', GaleriController::class);
 
 Route::get('/peternakan/print-report', [PeternakanController::class, 'printReport'])->name('peternakan.printReport');
